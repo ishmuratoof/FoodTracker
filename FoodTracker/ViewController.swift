c2ccc68d53210fbd5a64fc9ac3eb5f32ce3c1e35
Kamil <ishmuratoof@icloud.com>
2020-02-10 22:01:48 +0500

    Edit button added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -22,7 +22,16 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         nameTextField.delegate = self
+        
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
         
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
@@ -65,7 +74,18 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     //MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+          // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+          let isPresentingInAddMealMode = presentingViewController is UINavigationController
+          
+          if isPresentingInAddMealMode {
+              dismiss(animated: true, completion: nil)
+          }
+          else if let owningNavigationController = navigationController{
+              owningNavigationController.popViewController(animated: true)
+          }
+          else {
+              fatalError("The MealViewController is not inside a navigation controller.")
+          }
     }
     
     // This method lets you configure a view controller before it's presented.
